61e80b99f7043ae7bc04838f2013c12c6173913d
臭脚脚 <gassnake999@163.com>
2018-12-25 09:32:53 +0800

    Update js-lib.js

--- js-lib.js
+++ js-lib.js
@@ -103,3 +103,39 @@ function mergeArray(array) {
       return resAry
     }
   }
+
+
+/**
+ @description 快速排序
+ @author ruanyifeng(http://www.ruanyifeng.com)
+ @parmas {ar - 目标数组}
+*/
+var quickSort = function(arr) {
+
+　　if (arr.length <= 1) { return arr; }
+
+　　var pivotIndex = Math.floor(arr.length / 2);
+
+　　var pivot = arr.splice(pivotIndex, 1)[0];
+
+　　var left = [];
+
+　　var right = [];
+
+　　for (var i = 0; i < arr.length; i++){
+
+　　　　if (arr[i] < pivot) {
+
+　　　　　　left.push(arr[i]);
+
+　　　　} else {
+
+　　　　　　right.push(arr[i]);
+
+　　　　}
+
+　　}
+
+　　return quickSort(left).concat([pivot], quickSort(right));
+
+};
